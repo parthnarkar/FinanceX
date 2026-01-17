c0909077122c352b177e23bbb922c256c5b4595b
Tanish Jain <tanishjain020205@gmail.com>
2026-01-17 11:18:32 +0530

    URL Fixed

--- client/src/hooks/helper.js
+++ client/src/hooks/helper.js
@@ -1,9 +1,9 @@
 let backendUrl;
 
 if (process.env.NODE_ENV === 'production') {
-    backendUrl = import.meta.env.VITE_BACKEND_URL; 
+    backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://finance-x-backend.vercel.app'; 
 } else {
-    backendUrl = import.meta.env.VITE_BACKEND_URL_DEV;
+    backendUrl = import.meta.env.VITE_BACKEND_URL_DEV || 'http://localhost:5000';
 }
 
 export { backendUrl };
